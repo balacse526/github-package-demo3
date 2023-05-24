b3900f1d76288646654fec81449c3fd89c3200e9
Balakrishna <balacse526@gmail.com>
2023-05-24 21:09:29 +0530

    changes2

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,9 +1,1 @@
-import React from "react"
-import Button from "./Components/Button/Button"
-function App() {
-    return (<div>
-        <Button onClick={() => (alert('Save Button Clicked'))}>Reset Button</Button>
-    </div>)
-}
-
-export default App
+console.log("Hello..............")
