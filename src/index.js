e8b43f973b99a3921368179d208480691b8b0076
Balakrishna <balacse526@gmail.com>
2023-05-24 19:30:12 +0530

    changes3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
-import Hello from "./app"
+import App from "./app"
 
-ReactDom.render(<Hello />, document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'))
