288a3270c799054d32c50b3dd33600c6f14bd2b6
Balakrishna <balacse526@gmail.com>
2023-05-24 18:29:01 +0530

    changes2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Button from "./Components/Button/Button"
-function App() {
+function Hello() {
     return (<div>
         <Button onClick={() => (alert('Save Button Clicked'))}>Reset Button</Button>
     </div>)
 }
 
-export default App
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
-import App from "./app"
+import Hello from "./app"
 
-ReactDom.render(<App />, document.getElementById('app'))
+ReactDom.render(<Hello />, document.getElementById('app'))
